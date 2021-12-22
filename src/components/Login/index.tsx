82adaaf746133fead2c1e06e76d20be9ac15f53c
José Marinho <eduardo.w.marinho@example.com>
2021-12-22 16:27:26 +0000

    Completed login

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -1,4 +1,4 @@
-import React, { EventHandler, useContext, useState } from "react";
+import React, { useContext, useState } from "react";
 // Styles
 import { Wrapper } from "./Login.styles";
 // Context
