6b7a8e5d6e2f9e278fda5b3c63da6b98930f76cf
José Marinho <eduardo.u.marinho@gmail.com>
2021-12-29 17:53:02 +0000

    Login responsive

--- src/components/LoginBackground/index.tsx
+++ src/components/LoginBackground/index.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect } from "react";
 // Styles 
 import { Wrapper } from "./LoginBackground.styles";
 // Components
