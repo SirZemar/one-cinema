6faca4b2f5d8f72547399f6c4de281242658e38d
José Marinho <eduardo.u.marinho@gmail.com>
2022-01-04 17:05:50 +0000

    Fixed

--- src/config.ts
+++ src/config.ts
@@ -11,14 +11,14 @@ const REQUEST_TOKEN_URL: string = `${API_URL}authentication/token/new?api_key=${
 const LOGIN_URL: string = `${API_URL}authentication/token/validate_with_login?api_key=${API_KEY}`;
 const SESSION_ID_URL: string = `${API_URL}authentication/session/new?api_key=${API_KEY}`;
 
-const CONFIGURATION: string = `${API_URL}/configuration?api_key=${API_KEY}`;
+const CONFIGURATION: string = `${API_URL}configuration?api_key=${API_KEY}`;
 
 const IMAGE_BASE_URL: string = 'http://image.tmdb.org/t/p/';
 // Sizes: w300, w780, w1280, original
 const BACKDROP_SIZE: string = 'w1280';
 // w92, w154, w185, w342, w500, w780, original
 const POSTER_SIZE: string = 'w780';
-const PROFILE_SIZE: string = 'w200';
+const PROFILE_SIZE: string = 'original';
 
 export {
   SEARCH_BASE_URL,
