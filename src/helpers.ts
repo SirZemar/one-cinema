0cbfb8215a99d1bc4ec435cce3ade651eadacf79
José Marinho <eduardo.u.marinho@gmail.com>
2022-01-04 17:06:25 +0000

    Date formar and age calculation functions

--- src/helpers.ts
+++ src/helpers.ts
@@ -13,8 +13,39 @@ export const convertMoney = (money: number): string => {
   });
   return formatter.format(money);
 };
-
 export const isPersistedState = (stateName: string): any => {
   const sessionState = sessionStorage.getItem(stateName);
   return sessionState && JSON.parse(sessionState);
 };
+export const changeDateFormat = (prevDate: string) => {
+  const allMonths = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  const monthIndex = new Date(prevDate).getMonth();
+
+  const day = new Date(prevDate).getDate();
+  const month = allMonths[monthIndex];
+  const year = new Date(prevDate).getFullYear();
+
+  let ordinalNumber = '';
+  switch (day) {
+    case 1:
+      ordinalNumber = 'st';
+      break;
+    case 2:
+      ordinalNumber = 'nd';
+      break;
+    case 3:
+      ordinalNumber = 'rd';
+      break;
+    default:
+      ordinalNumber = 'th';
+      break;
+  }
+
+  return `${month} ${day}${ordinalNumber}, ${year}`
+};
+export const calculateAge = (birth: string, death: string, isDead: boolean) => {
+  const lastDate = isDead ? new Date(death).getTime() : Date.now();
+  const birthDay = new Date(birth).getTime();
+  const timeDifference = lastDate - birthDay;
+  return new Date(timeDifference).getUTCFullYear() - 1970;
+}
