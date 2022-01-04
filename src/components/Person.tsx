91d7644b9fdf9a3fcdb750f4891fc4d7b772b572
José Marinho <eduardo.u.marinho@gmail.com>
2022-01-04 17:07:54 +0000

    Person page

--- src/components/Person.tsx
+++ src/components/Person.tsx
@@ -3,33 +3,43 @@ import { useParams } from "react-router-dom";
 // Hook
 import { usePersonFetch } from "../hooks/usePersonFetch";
 // Config 
-import { IMAGE_BASE_URL, PROFILE_SIZE } from "../config";
+import { IMAGE_BASE_URL, PROFILE_SIZE, POSTER_SIZE } from "../config";
 // Components
 import BreadCrumb from "./BreadCrumb";
 import Spinner from "./Spinner";
-import ActorCard from "./ActorCard";
+import PersonInfo from "./PersonInfo";
+import Grid from "./Grid";
+import Thumb from "./Thumb";
+// Images 
+import NoImage from "../images/no_image.jpg"
 
 const Person: React.FC = () => {
     const { actorId, movieTitle, movieId }: any = useParams();
-    const { state: person, loading, error }: any = usePersonFetch(actorId);
-    console.log(error)
+    const { state: person, loading, error } = usePersonFetch(actorId ? actorId : '');
+    // console.log(person)
 
     const imageUrl = `${IMAGE_BASE_URL}${PROFILE_SIZE}${person.profile_path}`;
 
     if (loading) return <Spinner />
+    if (error) return <div>Something went wrong...</div>
+
     return (
         <>
-            <BreadCrumb movieTitle={movieTitle} movieId={movieId} actor={person.name} actorId={actorId} />
-            {<ActorCard name={person.name} character={null} imageUrl={imageUrl} actorId={actorId} movieTitle={movieTitle} movieId={movieId} clickable={false} hoverable={false} />}
-            <div style={{ maxWidth: 'var(--maxWidth)', margin: '0 auto' }}>
-                <h1 style={{ color: 'black' }}>Actor</h1>
-                <h1 style={{ color: 'black' }}>{actorId}</h1>
-                <h1 style={{ color: 'black' }}>{person.name}</h1>
-                <h2 style={{ color: 'black', display: 'inline' }}>{person.birthday} </h2>
-                <h2 style={{ color: 'black', display: 'inline' }}>- {person.deathday ? person.deathday : 'today'}</h2>
-                <p style={{ color: 'black' }}>{person.biography}</p>
-                <img src={`${IMAGE_BASE_URL}${PROFILE_SIZE}${person.profile_path}`} alt={person.name} />
-            </div>
+            {actorId && movieTitle && movieId && <BreadCrumb movieTitle={movieTitle} movieId={movieId} actor={person.name} actorId={actorId} />}
+            <PersonInfo person={person} imageUrl={imageUrl} />
+            <Grid header='Movies'>
+                {person.credits && person.credits.map(movie => (
+                    <Thumb
+                        image={movie.poster_path
+                            ? IMAGE_BASE_URL + POSTER_SIZE + movie.poster_path
+                            : NoImage}
+                        clickable
+                        key={movie.id}
+                        movieId={movie.id}
+                    />
+                ))}
+            </Grid>
+            {/* {loading && <Spinner />} */}
         </>
     )
 };
