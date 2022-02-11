e13b0c5a38aba679fd15b9ac01b0d7172b38c0fd
José Marinho <eduardo.u.marinho@gmail.com>
2022-02-11 12:02:09 +0000

    Dimensions config

---
+++ src/dimensionsConfig.ts
@@ -0,0 +1,18 @@
+export const dimensions = {
+    size: {
+        small: '1rem',
+        med: '1.2rem',
+        big: '1.5rem',
+        bigger: '2rem',
+        biggest: '2.3rem',
+    },
+    break: {
+        XXL: '1500px',
+        XL: '1200px',
+        L: '917px',
+        M: '768px',
+        S: '600px',
+        XS: '500px',
+        XXS: '360px',
+    }
+};
