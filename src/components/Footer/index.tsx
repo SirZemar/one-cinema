0be677199574a9f31489c715cd12c3d34b8e3f5a
José Marinho <eduardo.u.marinho@gmail.com>
2022-02-11 13:51:30 +0000

    Loading context

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
+// Context
+import { LoadingContext } from "../../context";
 // Styles
 import { Wrapper, Content } from './Footer.styles';
 
-const Footer: React.FC = () => (
-    <Wrapper>
-        <Content>
-            <p>José Marinho</p>
-        </Content>
-    </Wrapper>
-);
+const Footer: React.FC = () => {
+
+    const [loadingGlobal] = useContext(LoadingContext);
+
+    return (
+        <>
+            {!loadingGlobal && <Wrapper>
+                <Content>
+                    <p>José Marinho</p>
+                </Content>
+            </Wrapper>}
+        </>
+    )
+};
 
 export default Footer;
